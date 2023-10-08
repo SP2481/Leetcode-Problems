11c780fdb6a43f1d15011aefa243a19d634edcf1
SP2481 <96459036+SP2481@users.noreply.github.com>
2023-10-08 15:01:02 +0530

    Time: 1 ms (10.71%), Space: 41.2 MB (72.66%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,14 +15,29 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        // DFS rescursive
+        // LEVEL ORDER TRAVERSAL
         if(root == null){
             return 0;
         }
-        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
-
-   
-
-        
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
+        int level = 0; // Initialize level to 0
+    
+        while (!q.isEmpty()) { // Check if the queue is empty
+            int size = q.size(); // Get the current level's node count
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            level++; // Increment the level after processing each level's nodes
+        }
+    
+        return level;
+  
     }
 }
